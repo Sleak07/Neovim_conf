8c3ef8938b9ef9223b42e572dfb9c0a98e1eaa15
Bimal Mallick <trisky650@gmail.com>
2024-05-24 14:20:55 +0530

    Added mason and Dap

---
+++ lua/plugins/comments.lua
@@ -0,0 +1,8 @@
+-- Comment/Uncomment Lines of Code
+return {
+    -- https://github.com/tpope/vim-commentary
+    'tpope/vim-commentary',
+    'szw/vim-maximizer', -- Maximize and restore current window
+    event = 'VeryLazy',
+  }
+
